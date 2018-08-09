8fc9948e8d5537d7ae2a389baf9fad73b672eed4
Andreas Dziemba <adziemba@web.de>
2018-08-09 14:14:07 +0200

    Update dbConfig.php

--- core/classes/datastorage/dbConfig.php
+++ core/classes/datastorage/dbConfig.php
@@ -1,13 +1,13 @@
 <?php
 
-define('dnguserdb', 'mysql:host=localhost;dbname=dnguser');
-define('dnguserdbuser', 'dng_secure');
-define('dnguserdbpw', 'dng');
+define('dnguserdb', 'mysql:host=localhost;dbname=dbname');
+define('dnguserdbuser', 'dng_user');
+define('dnguserdbpw', 'password');
 
-define('dnglcarsdb', 'mysql:host=localhost;dbname=dnglcars');
-define('dnglcarsdbuser', 'dng');
-define('dnglcarsdbpw', 'dng');
+define('dnglcarsdb', 'mysql:host=localhost;dbname=dbname');
+define('dnglcarsdbuser', 'dng_user');
+define('dnglcarsdbpw', 'password');
 
-define('dngdatadb', 'mysql:host=localhost;dbname=dngdata');
-define('dngdatadbuser', 'dng_data');
-define('dngdatadbpw', 'dng');
+define('dngdatadb', 'mysql:host=localhost;dbname=dbname');
+define('dngdatadbuser', 'dng_user');
+define('dngdatadbpw', 'password');
